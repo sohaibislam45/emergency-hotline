9f3a5e1a1ca48a32ca3b118eb0980c5dd4f0e53e
Sohaibislam45 <sohaibislam45@gmail.com>
2025-08-29 20:39:52 +0600

    added all the ramain functionalities of this website

--- script/script.js
+++ script/script.js
@@ -21,3 +21,83 @@ for (let icon of heartIcons) {
         heartCountEl.innerText = heartCount;
     });
 }
+
+// coin management feature
+const coinCountEl = document.getElementById("coin-count");
+let coins = 100;
+coinCountEl.innerText = coins;
+
+// call buttons feature
+const callButtons = document.getElementsByClassName("card-call");
+
+for (let callBtn of callButtons) {
+    callBtn.addEventListener("click", function () {
+        const card = this.closest(".card");
+        const serviceName = card.getElementsByClassName("service-name")[0].innerText;
+        const serviceNumber = card.getElementsByClassName("service-number")[0].innerText;
+
+        if (coins < 20) {
+            alert("Not enough coins to make a call!");
+            return;
+        }
+
+        coins -= 20;
+        coinCountEl.innerText = coins;
+
+        alert("📞... Calling " + serviceName + " (" + serviceNumber + ")");
+
+        const historyContainer = document.getElementById("call-history");
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const formattedHours = hours % 12 || 12;
+        const timeStr = formattedHours + ":" + minutes + ":" + seconds + " " + ampm;
+
+        const div = document.createElement("div");
+        div.className = "flex justify-between items-center bg-[#fafafa] p-3 mt-2 rounded-lg";
+
+        div.innerHTML = `
+            <div>
+                <h1 class="text-lg font-bold max-w-[215px]">${serviceName}</h1>
+                <p class="text-lg text-[#5c5c5c]">${serviceNumber}</p>
+            </div>
+            <p>${timeStr}</p>
+        `;
+
+        historyContainer.prepend(div);
+    });
+}
+
+
+// clear history funtionality
+const clearHistoryBtn = document.getElementById("clear-history");
+const callHistoryList = document.getElementById("call-history");
+
+clearHistoryBtn.addEventListener("click", function () {
+    callHistoryList.innerHTML = ""; 
+    alert("Call history cleared!");
+});
+
+
+
+// copy button functionality
+
+let copyCount = 0;
+const copyButtons = document.getElementsByClassName("btn");
+
+for (let copyBtn of copyButtons) {
+    copyBtn.addEventListener("click", function () {
+        if (this.innerText.includes("Copy")) {
+            const card = this.closest(".card");
+            const serviceName = card.getElementsByClassName("service-name")[0].innerText;
+            const serviceNumber = card.getElementsByClassName("service-number")[0].innerText;
+
+            navigator.clipboard.writeText(serviceNumber);
+            copyCount++;
+
+            alert("Copied " + serviceName + " (" + serviceNumber + ") to clipboard! Total copies: " + copyCount);
+        }
+    });
+}
